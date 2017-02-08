7d7efa79f9da0f5b209620f0904263fcc575c07c
Thomas Minshull <thomasminshull@gmail.com>
2017-02-07 16:25:42 -0800

    initial commit

---
+++ Boxes2/Boxes2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Boxes2
+//
+//  Created by thomas minshull on 2017-02-07.
+//  Copyright © 2017 thomas minshull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
